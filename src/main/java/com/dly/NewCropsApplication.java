0df94d38e5afb99a62982e527a6d529d846c8549
ly286 <dlylove286@gmail.com>
2024-03-26 18:47:17 +0800

    first

---
+++ src/main/java/com/dly/NewCropsApplication.java
@@ -0,0 +1,13 @@
+package com.dly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewCropsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewCropsApplication.class, args);
+    }
+
+}
